7eb7a0475eba62421b54070e5b58b3764c62c1cb
Bushra Bilal <bushrabilal@gmail.com>
2021-10-28 13:16:32 -0400

    continued

--- store/urls.py
+++ store/urls.py
@@ -9,3 +9,24 @@ urlpatterns = [
     path('checkout/', views.checkout, name="checkout"),
 
 ]
+
+#
+# application - python back en d- front html/css
+# tempalt - html
+# static - css, images.
+# django-admin
+
+# django framwork -  django  , pyton  Flask ,
+#
+# language python +  sql lite
+#
+# programming langauge
+#
+# path
+#
+# website
+# html/css/js/images/icon / bootstra/jqyer
+#
+# python vs php vs java   vs javascript
+# django vs laraval vs spring vs  MERN stack
+
